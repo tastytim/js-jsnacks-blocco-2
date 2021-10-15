6f688f9b68e51d2073d80f262e7ade66649e71af
tastytim <tastytimgm@gmail.com>
2021-10-15 17:30:52 +0200

    commit fix

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 
-const array1 = [15, 2, 7];
-const array2 = [5];
+let array1 = [15, 2, 7];
+let array2 = [5 , 5 , 5 , 5];
 
 // Prendiamo un altra variabile e calcoliamo qual e dei due array piu piccolo
 
@@ -11,12 +11,15 @@ let count = array1.length - array2.length;
 if(count < 0){
     // concateniamo 
     for(let i = array1.length; i < array2.length; i++){
-        array1[i] = array2[i];
+        array1.push(array2[i]);
     }
     
 }else{
     for(let i = array2.length; i < array1.length ; i++){
-        array2[i] = array1[i];
+        array2.push(array1[i]);
     }
 }
+
+console.log(array2);
+console.log(array1);
 
