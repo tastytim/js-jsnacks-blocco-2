8cdcddf2b5bc1c692666537c5b65c38d0f5a6c02
tastytim <tastytimgm@gmail.com>
2021-10-15 17:05:57 +0200

    commit array

---
+++ array.js
@@ -0,0 +1,18 @@
+
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+// Prendiamo un altra variabile e calcoliamo qual e dei due array piu piccolo
+
+let count = array1.length - array2.length;
+
+// Se ad esempio e negativo 
+
+if(count < 0){
+    // concateniamo 
+    for(let i = array1.length;;){
+
+    }
+    
+}
+
